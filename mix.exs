7b9dd777ad72989a06be6baf272513d5b9787c45
Christopher Coté <Christopher Cot>
2018-04-09 13:39:24 -0500

    update ieq_gateway lib

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule RosettaHomeIeqSensor.Mixfile do
 
   defp deps do
     [
-      {:ieq_gateway, "~> 0.1.7"},
+      {:ieq_gateway, "~> 0.1.9"},
       {:cicada, github: "rosetta-home/cicada",  optional: true}
     ]
   end
